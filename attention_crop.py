8c38a24bed4b32cdc1f30d4f77e77483041d1ace
michael <michael.origin.j@gmail.com>
2017-08-09 22:41:54 +0800

    forward pass and gradient for AttentionCrop implemented in python

---
+++ attention_crop.py
@@ -0,0 +1,175 @@
+import tensorflow as tf
+
+
+def get_corners(param):
+    param = tf.convert_to_tensor(param, dtype=tf.float32)
+    tx, ty, tl = param[:, 0], param[:, 1], param[:, 2]
+    tx_tl = tf.maximum(tx - tl, 0.)
+    ty_tl = tf.maximum(ty - tl, 0.)
+    tx_br = tf.minimum(tx + tl, 1.)
+    ty_br = tf.minimum(ty + tl, 1.)
+    return tf.transpose(tf.convert_to_tensor([tx_tl, ty_tl, tx_br, ty_br]))
+
+
+def attention_crop(images, params, out_image_size, name=None):
+    with tf.name_scope(name, "AttentionCrop", [images, params, out_image_size]) as scope:
+        images = tf.convert_to_tensor(images, name="images")
+        params = tf.convert_to_tensor(params, name="params")
+        params = params / images.get_shape().as_list()[1]
+        crop_size = tf.convert_to_tensor([out_image_size, out_image_size], name="crop_size")
+        boxes = get_corners(params)
+        box_ind = tf.scan(lambda a, _: a + 1, params, -1)
+        out_images = tf.image.crop_and_resize(images, boxes, box_ind, crop_size)
+        return out_images
+
+k = 0.05
+h = lambda x: tf.divide(1.0, tf.add(1.0, tf.exp(tf.multiply(x, -k))))
+def diff_h(x):
+    return k * tf.exp(tf.multiply(-k, x)) / ((1 + tf.exp(-k * x)) *
+                                             (1 + tf.exp(-k * x)))
+
+
+def f(tx, ty, tl, x, y):
+    tx = tf.convert_to_tensor(tx, dtype=tf.float32)
+    ty = tf.convert_to_tensor(ty, dtype=tf.float32)
+    tl = tf.convert_to_tensor(tl, dtype=tf.float32)
+    x = tf.convert_to_tensor(x, dtype=tf.float32)
+    y = tf.convert_to_tensor(y, dtype=tf.float32)
+    return (h(x - (tx - tl)) -
+            h(x - (tx + tl))
+            ) * (
+        h(y - (ty - tl)) -
+        h(y - (ty + tl))
+    )
+
+
+def diff_f_tx(tx, ty, tl, x, y):
+    return (diff_h(x - (tx - tl)) -
+            diff_h(x - (tx + tl))
+            ) * (
+        h(y - (ty - tl)) -
+        h(y - (ty + tl))
+    )
+
+
+def diff_f_ty(tx, ty, tl, x, y):
+    return (diff_h(y - (ty - tl)) -
+            diff_h(y - (ty + tl))
+            ) * (
+        h(x - (tx - tl)) -
+        h(x - (tx + tl))
+    )
+
+def diff_f_tl(tx, ty, tl, x, y):
+    return -((diff_h(y - (ty - tl)) + diff_h(y - (ty + tl))) * (h(x - (tx - tl)) - h(x - (tx + tl))) + (
+    diff_h(x - (tx - tl)) + diff_h(x - (tx + tl))) * (h(y - (ty - tl)) - h(y - (ty + tl)))) + 0.005
+
+
+def normalize_grad(grad):
+    height, width, channel = grad.get_shape().as_list()
+    channel_max = tf.reduce_max(grad, axis=[0, 1])
+    c1 = tf.cond(channel_max[0] > 0., lambda: tf.multiply(tf.ones([height, width]), channel_max[0]),
+                 lambda: tf.ones([height, width]))
+    c2 = tf.cond(channel_max[1] > 0., lambda: tf.multiply(tf.ones([height, width]), channel_max[1]),
+                 lambda: tf.ones([height, width]))
+    c3 = tf.cond(channel_max[2] > 0., lambda: tf.multiply(tf.ones([height, width]), channel_max[2]),
+                 lambda: tf.ones([height, width]))
+    c_max = tf.stack([c1, c2, c3], axis=2)
+    normalized = grad / c_max
+    sumed = tf.reduce_sum(normalized, axis=2, keep_dims=True)
+    return sumed
+
+
+def get_grad_f(param): ## compute on the region
+    tx, ty, tl = param[0], param[1], param[2]
+    # create a tensor first
+    indices_x = tf.range(tx - tl, tx + tl + 1)
+    indices_y = tf.range(ty - tl, ty + tl + 1)
+
+    bottom2top_x = tf.map_fn(lambda i: i, indices_x)
+    layer_x = tf.map_fn(lambda i: bottom2top_x, indices_x)
+    bottom2top_y = tf.map_fn(lambda i: i, indices_y)
+    layer_y = tf.map_fn(lambda i: bottom2top_y, indices_y)
+    layer_y = tf.transpose(layer_y)
+    indices = tf.stack([layer_x, layer_y], axis=2)
+    gradients_f_tx = tf.map_fn(lambda row:
+                               tf.map_fn(lambda col: diff_f_tx(tx, ty, tl, col[0], col[1]), row),
+                               indices)
+    gradients_f_ty = tf.map_fn(lambda row:
+                               tf.map_fn(lambda col: diff_f_ty(tx, ty, tl, col[0], col[1]), row),
+                               indices)
+    gradients_f_tl = tf.map_fn(lambda row:
+                               tf.map_fn(lambda col: diff_f_tl(tx, ty, tl, col[0], col[1]), row),
+                               indices)
+
+    return tf.stack([gradients_f_tx, gradients_f_ty, gradients_f_tl], axis=2)
+
+
+#    for x in tf.range(tx - tl, tx + tl + 1):
+#        for y in tf.range(ty - tl, ty + tl + 1):
+#            diff_f_tx(tx, ty, tl, x, y)
+#            diff_f_ty(tx, ty, tl, x, y)
+#            diff_f_tl(tx, ty, tl, x, y)
+#
+def get_grad_f_helper(out_size):
+    def get_grad_f2(param):
+        tx, ty, tl = param[0], param[1], param[2]
+        ranges = tf.range(out_size)
+        bottom2top = tf.map_fn(lambda i: i, ranges)
+        layer_x = tf.map_fn(lambda i: bottom2top, ranges)
+        layer_y = tf.transpose(layer_x)
+        layer_x = tx - tl + 2 * tl / out_size * layer_x
+        layer_y = ty - tl + 2 * tl / out_size * layer_y
+        indices = tf.stack([layer_x, layer_y], axis=2)
+        gradients_f_tx = tf.map_fn(lambda row:
+                                   tf.map_fn(lambda col: diff_f_tx(tx, ty, tl, col[0], col[1]), row),
+                                   indices)
+        gradients_f_ty = tf.map_fn(lambda row:
+                                   tf.map_fn(lambda col: diff_f_ty(tx, ty, tl, col[0], col[1]), row),
+                                   indices)
+        gradients_f_tl = tf.map_fn(lambda row:
+                                   tf.map_fn(lambda col: diff_f_tl(tx, ty, tl, col[0], col[1]), row),
+                                   indices)
+
+        return tf.stack([gradients_f_tx, gradients_f_ty, gradients_f_tl], axis=2)
+    return get_grad_f2
+
+
+
+
+@tf.RegisterGradient("AttentionCrop")
+def _attention_crop_grad(op, grad):
+    images = op.inputs[0]
+    params = op.inputs[1]
+    out_image_size = op.inputs[2]
+
+    top_diff = grad
+    top_data = op.outputs[0]
+    grad_to_params = tf.zeros_like(params)
+    input_image_size = images.get_shape().as_list[2]
+    # count1, count2, count3 ## what are these guys
+    # top is exactly an image of three channels
+    grad2 = tf.abs(grad)
+    #channel_max = tf.reduce_max(grad2, axis=[1, 2])
+    grad_normalized = tf.map_fn(normalize_grad, grad2)
+    grad_normalized = grad_normalized * 0.0000001
+    out_size = grad.get_shape().as_list[2]
+    assert out_image_size == out_size
+    get_grad_f2 = get_grad_f_helper(out_size)
+    derivatives_f = tf.map_fn(get_grad_f2, params)
+    grad_product = derivatives_f * grad_normalized
+    grad_to_params = tf.reduce_sum(grad_product, axis=[0, 1])
+
+    #for each of the sample:
+        # obtain tx, ty and tl
+        # find max absolute value in top grads with each channel
+        # loop through indices of the top image
+            # convert grad to their absolute value
+            # set grad to grad / max(grab_with_channel) * 0.0000001
+            # get coordinates on the original bottom image
+            # update gradients to each of the three params
+    return tf.zeros_like(images), grad_to_params, tf.zeros_like(out_image_size)
+
+
+if __name__ == "__main__":
+    pass
---
+++ problem_unittests.py
@@ -0,0 +1,149 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pylab as plt
+from attention_crop import *
+
+
+k = 0.05
+def _print_success_message():
+    print('Tests Passed')
+
+
+def test_get_corners(get_corners):
+    with tf.Graph().as_default():
+        params = np.array([[0.5, 0.5, 0.3],
+                           [0.2, 0.2, 0.3],
+                           [0.8, 0.8, 0.3],
+                           [0.5, 0.2, 0.3],
+                           [0.8, 0.2, 0.3]])
+        params2 = np.array([0.5, 0.5, 0.3])
+        boxes = get_corners(params)
+        # boxes2 = get_corners(params2)
+        assert isinstance(boxes, tf.Tensor), 'boxes is not a Tensor.'
+        # print(boxes)
+        assert boxes.get_shape().as_list() == [params.shape[0], 4], "boxes shape is not right."
+        # assert boxes2.get_shape().as_list() == [params2.shape[0], 4]
+        with tf.Session() as sess:
+            correct_result = np.zeros((params.shape[0], 4), np.float32)
+            correct_result[:, 0] = np.maximum(params[:, 0] - params[:, 2], 0)
+            correct_result[:, 1] = np.maximum(params[:, 1] - params[:, 2], 0)
+            correct_result[:, 2] = np.minimum(params[:, 0] + params[:, 2], 1)
+            correct_result[:, 3] = np.minimum(params[:, 1] + params[:, 2], 1)
+            assert np.allclose(boxes.eval(), correct_result)
+
+    _print_success_message()
+
+
+def test_attention_crop(attention_crop):
+    with tf.Graph().as_default():
+        image11 = np.zeros((20, 20))
+        image12 = np.ones((20, 20))
+        image21 = np.ones((20, 20))
+        image22 = np.zeros((20, 20))
+        image1 = np.concatenate((image11, image12), axis=1)
+        image2 = np.concatenate((image21, image22), axis=1)
+        image = np.concatenate((image1, image2))
+        image = np.stack((image, image, image), axis=-1)
+        image = np.expand_dims(image, axis=0)
+
+        params = np.array([[0.3, 0.3, 0.3]], dtype=np.float32)
+        output_size = 40
+        image_out = attention_crop(image, params, output_size)
+        # todo: a robust way to test the attention_crop correctness
+
+
+def test_h(h):
+    with tf.Graph().as_default():
+        x = np.array([5.0, 6.0, 7.0, 8.0], dtype=np.float32)
+        tfx = tf.constant(x)
+        tfy = h(tfx)
+        y = np.divide(1.0, np.add(1.0, np.exp(np.multiply(x, -k))))
+        with tf.Session() as sess:
+            assert np.allclose(y, tfy.eval())
+
+
+def test_f(f):
+    with tf.Graph().as_default():
+        input1 = [5.0, 5, 3, 4, 2]
+        tf_output1 = f(*input1)
+        with tf.Session() as sess:
+            print(tf_output1.eval())
+
+def test_reduce_max():
+    zeros = np.zeros((5, 5)) + 0.01
+    ones = np.ones((5, 5))
+    twos = np.ones((5, 5)) * 2
+
+    values = np.stack([zeros, ones, twos], axis=2)
+    values = np.expand_dims(values, axis=0)
+    values2 = values * 2
+    values = np.concatenate((values, values2), axis=0)
+    channel_max = tf.reduce_max(values, axis=[1, 2])
+    print(values)
+    print("shape is {}".format(values.shape))
+    with tf.Session() as sess:
+        print("channel max is {}".format(channel_max.eval()))
+        print("channel max shape is {}".format(channel_max.eval().shape))
+
+def test_normalize_grad(normalize_grad):
+    image_input = tf.Variable([[[2., 6, 15], [1, 2, 3]],
+                               [[1, 2, 3], [1, 2, 3]],
+                               ])
+    init = tf.global_variables_initializer()
+    normalized = normalize_grad(image_input)
+    with tf.Session() as sess:
+        sess.run(init)
+        print(normalized.eval())
+
+
+def test_variable():
+    v = tf.Variable([1, 2])
+    init = tf.global_variables_initializer()
+    with tf.Session() as sess:
+        sess.run(init)
+        print(v.eval())
+
+def test_get_derivatives(get_derivatives):
+    with tf.Graph().as_default():
+        param = [5, 8., 3]
+        param = tf.convert_to_tensor(param)
+        result = get_derivatives(param)
+        with tf.Session() as sess:
+            print(result.eval())
+            print("shape is {}".format(result.eval().shape))
+
+def test_multiply_broadcast():
+    with tf.Graph().as_default():
+        ones = tf.ones((2, 2))
+        twos = ones * 2
+        threes = ones * 3
+        packed = tf.stack([ones, twos, threes], axis=2)
+        twos_expanded = tf.expand_dims(twos, axis=-1)
+        product = tf.multiply(packed, twos_expanded)
+        with tf.Session() as sess:
+            print(product.eval())
+
+
+
+def all_main():
+    test_reduce_max()
+    test_variable()
+    test_get_corners(get_corners)
+    test_attention_crop(attention_crop)
+    test_h(h)
+    test_f(f)
+    test_normalize_grad(normalize_grad)
+
+def main():
+    test_get_derivatives(get_grad_f)
+    get_grad_f2 = get_grad_f_helper(10.)
+    test_get_derivatives(get_grad_f2)
+    test_multiply_broadcast()
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
